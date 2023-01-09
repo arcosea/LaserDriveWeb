8fe155f0d59a2c7915db3573bf29f53d0fe1b98c
Erick Arcos <arcosea@plu.edu>
2023-01-09 14:07:37 -0800

    setup basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
+
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route, Routes, NavLink} from "react-router-dom";
+import Navbar from './NavBar';
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Sprints } from './pages/Sprints';
 
 function App() {
-  return (
+  return (  
+  <>
+    <Navbar />
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={< Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/sprints" element={<Sprints />} />
+        
+
+      </Routes>
     </div>
+  </>
   );
+
 }
 
 export default App;
